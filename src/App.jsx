fc08a28f787790dadf5e71ffee76de492a132e3b
shrutagarwal <shrutagarwal@microsoft.com>
2025-11-25 14:13:45 +0530

    Deploy build to gh-pages

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import PagesDropdown from "./components/PagesDropdown/PagesDropdown";
-
-export default function App() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <PagesDropdown />
-    </div>
-  );
-}
